769fe541ef6ff2cfb0d1e61a2a293871e791db39
ShamKedar <shamkedar2003@gmail.com>
2026-08-01 01:26:58 +0530

    Updated the fetch route.

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ async function sendMessage() {
 
     try {
 
-        const response = await fetch("http://127.0.0.1:5000/chat", {
+        const response = await fetch("https://portfolio-bot-15wq.onrender.com/chat", {
 
             method: "POST",
 
